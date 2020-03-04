c40d6b1dbada92f4638767662d43601bfacb232c
Anish Moorthy <anlsh@protonmail.com>
2020-03-03 22:55:13 -0500

    Define free variables "correctly"

--- lang_ops.v
+++ lang_ops.v
@@ -20,7 +20,6 @@ Fixpoint substitute (e : expr) (s : expr) (x : var) : expr :=
   end.
 
 
-(* TODO I'm sure that I'm not defining free variables (in exprs and configs) correctly *)
 Fixpoint expr_freevars (e : expr) : VarSet.t :=
   match e with
   | evar y => VarSet.singleton y
@@ -33,8 +32,8 @@ Fixpoint config_freevars (c : config) : VarSet.t :=
   match c with
   | (c1 $$ c2) => VarSet.union (config_freevars c1) (config_freevars c2)
   | x ** c => VarSet.diff (config_freevars c) (VarSet.singleton x)
-  | cellmake x v => VarSet.empty
-  | (x <- e) => VarSet.diff (expr_freevars e) (VarSet.singleton x)
-  | h ~ x => VarSet.empty
-  | h ~ used => VarSet.empty
+  | cellmake x v => VarSet.union (VarSet.singleton x) (expr_freevars v)
+  | (x <- e) => VarSet.union (expr_freevars e) (VarSet.singleton x)
+  | h ~ x => VarSet.union (VarSet.singleton h) (VarSet.singleton x)
+  | h ~ used => VarSet.singleton h
   end.
